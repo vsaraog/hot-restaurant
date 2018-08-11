94324ad1cf365400bcd37b96f34d282497ce3709
Susanta Mozumder <sushanta.mozumder@gmail.com>
2018-08-11 13:14:53 -0400

    added tables.js code

--- tables.js
+++ tables.js
@@ -1,2 +1,24 @@
-var current = "";
-var wait = "";
+$.ajax({
+    url: '/api',
+    method: 'GET'
+  }).done(function(response) {
+    console.log(response.reservations);
+    if (response) {
+      var tableSection = $('#tableSection');
+      var waitlistSection = $('#waitlistSection');
+      for (var i = 0; i < response.reservations.length; i++) {
+        var tableDiv = $('<div>').addClass('well');
+        var id = response.reservations[i].id;
+        tableDiv.append($('<h2>').html("<span class='label label-primary'>1</span>|"));
+        tableDiv.append($('<div>').append($('<h4>').text(id)));
+        tableSection.append(tableDiv);
+      }
+      for (var i = 0; i < response.waitlist.length; i++) {
+        var tableDiv = $('<div>').addClass('well');
+        var id = response.waitlist[i].id;
+        tableDiv.append($('<h2>').html("<span class='label label-primary'>1</span>|"));
+        tableDiv.append($('<div>').append($('<h4>').text(id)));
+        waitlistSection.append(tableDiv);
+      }
+    }
+  });
