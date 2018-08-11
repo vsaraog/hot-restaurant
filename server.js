f9fdad7ff243265a8083401c5a451a65c03e5ecc
vsaraogi <mailvsaraogi@gmail.com>
2018-08-11 11:43:08 -0400

    set up the server

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 var express = require("express"); 
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+const HOMEPAGE = "index.html";
+
+app.get("/", (req, resp) => {
+    res.sendFile(path.join(__dirname, HOMEPAGE))
+})
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log("App listening on PORT: http://localhost:" + PORT);
+})
